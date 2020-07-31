d7f917a63c607b766845fd2dccf90ef364f62006
PULOK TARAFDER <pulok.tarafder@gmail.com>
2020-07-31 23:30:08 +0600

    new file

---
+++ loop_continue_break.py
@@ -0,0 +1,24 @@
+# n=5
+# while n > 0:
+#     print(n)
+#     n=n-1
+# print('Blastoff!')
+# print(n)
+
+# while True:
+#     line = input ('> ')
+#     if line[0] == '#' : #if the input starts with #, it wont print anything.
+#         continue
+#     if line == 'done' :
+#         break
+#     print(line)
+# print('Done')
+
+for i in [5, 4, 3, 2, 1]:
+    print(i)
+print('DONE')
+
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends:
+    print('Happy New Year!', friend)
+print('Done!')
